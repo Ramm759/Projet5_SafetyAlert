1ada17fd886ba8751906701ca5ee6baf7f554af1
Ramm759 <fredgsn59@gmail.com>
2021-01-08 19:05:42 +0100

    Test PersonControlleur (pb sur update)

--- src/test/java/com/mycompany/safetyAlert/controller/PersonControllerTest.java
+++ src/test/java/com/mycompany/safetyAlert/controller/PersonControllerTest.java
@@ -23,6 +23,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 @AutoConfigureMockMvc
 public class PersonControllerTest {
     @Autowired
+    // MockMvc est un module de SpringTest permettant de simplifier la création de tests Rest
     MockMvc mockMvc;
 
     @MockBean
@@ -80,6 +81,50 @@ public class PersonControllerTest {
 
         // THEN
         mockMvc.perform(MockMvcRequestBuilders.post("/person").contentType(MediaType.APPLICATION_JSON).content(jsonPerson.toString())).andExpect(MockMvcResultMatchers.status().isConflict());
+
+    }
+
+    // Modification des personnes
+    @Test
+    void updatePersonValid() throws Exception {
+        // GIVEN
+        ObjectMapper obm = new ObjectMapper();
+        ObjectNode jsonPerson = obm.createObjectNode(); // on prépare le Json vide
+        jsonPerson.set("firstName", TextNode.valueOf(firstnameTest));
+        jsonPerson.set("lastName", TextNode.valueOf(lastnameTest));
+
+        // WHEN
+
+        // THEN
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/person").contentType(MediaType.APPLICATION_JSON).content(jsonPerson.toString())).andExpect(MockMvcResultMatchers.status().isNoContent());
+
+    }
+
+    @Test
+    void updatePersonInvalid() throws Exception {
+        // GIVEN
+        ObjectMapper obm = new ObjectMapper();
+        ObjectNode jsonPerson = obm.createObjectNode(); // on prépare le Json vide
+        jsonPerson.set("firstName", TextNode.valueOf(firstnameTest));
+        jsonPerson.set("lastName", TextNode.valueOf(""));
+        // WHEN
+
+        // THEN
+        mockMvc.perform(MockMvcRequestBuilders.put("/person").contentType(MediaType.APPLICATION_JSON).content(jsonPerson.toString())).andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
+
+    @Test
+    void updatePersonWhenPersonNotExist() throws Exception {
+        // GIVEN
+        ObjectMapper obm = new ObjectMapper();
+        ObjectNode jsonPerson = obm.createObjectNode(); // on prépare le Json vide
+        jsonPerson.set("firstName", TextNode.valueOf(firstnameTest));
+        jsonPerson.set("lastName", TextNode.valueOf(lastnameTest));
+        // WHEN
+
+        // THEN
+        mockMvc.perform(MockMvcRequestBuilders.put("/person").contentType(MediaType.APPLICATION_JSON).content(jsonPerson.toString())).andExpect(MockMvcResultMatchers.status().isNotFound());
 
     }
 }
